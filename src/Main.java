fb99df2bebefd51285780569b313fad390f1ac8a
Debuga Dev <debugadev@gmail.com>
2023-10-18 09:34:41 -0300

    Curso de Java para iniciantes 2023 - IF - ELSE IF - ELSE e Operadores

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Byte b = Byte.valueOf((byte) 1);
-        Short s = Short.valueOf((short) 2);
-        Integer x = Integer.valueOf(3);
-        Long l = Long.valueOf(4);
 
-        Float f = Float.valueOf((float) 2.6);
-        Double d = Double.valueOf(3.7);
+        /*
+        == Iguais;
+        != Diferente;
+        >  Maior;
+        >= Maior ou Igual;
+        <  Menor;
+        <= Menor ou Igual;
+        */
 
-        Boolean bo = Boolean.valueOf(true);
-        Character c = Character.valueOf('D');
+        boolean condicao = 1 <= 1;
+        int idade = 16;
 
-        float ii = x.floatValue();
-        System.out.println(ii);
-
-        int fi = f.intValue();
-        System.out.println(fi);
-
-        int i1 = 1000;
-        int i2 = 2000;
-
-        if (i1 == i2) {
-            System.out.println("Iguais");
+        if (idade >= 65) {
+            System.out.println("Idoso");
+        } else if (idade >= 18) {
+            System.out.println("Maior de idade");
         } else {
-            System.out.println("Diferentes");
+            System.out.println("Menor de idade");
         }
-
-        Integer int1 = Integer.valueOf(1000);
-        Integer int2 = Integer.valueOf(1000);
-
-        if (int1.equals(int2)) {
-            System.out.println("Iguais");
-        } else {
-            System.out.println("Diferentes");
-        }
-
-        Integer int3 = 2000;
-        int i3 = int3;
-
-        System.out.println(int3 + " " + i3);
-
-        recebeInt(int3);
-        recebeInteger(i3);
-    }
-
-    public static void recebeInt(int i) {
-        System.out.println("int: " + i);
-    }
-
-    public static void recebeInteger(Integer i) {
-        System.out.println("Integer: " + i);
     }
 }
