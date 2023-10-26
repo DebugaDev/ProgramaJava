408f01c7a33cd44edebb3d8627eb85cc7f80c273
Debuga Dev <debugadev@gmail.com>
2023-10-26 12:23:11 -0300

    Curso de Java para iniciantes 2023 - Switch e IF

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,73 @@
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        == Iguais;
-        != Diferente;
-        >  Maior;
-        >= Maior ou Igual;
-        <  Menor;
-        <= Menor ou Igual;
-        */
+        Integer idadePedro = 21;
+        Integer idadeJoao = 21;
+        String nome = "Pedro";
 
-        boolean condicao = 1 <= 1;
-        int idade = 16;
-
-        if (idade >= 65) {
-            System.out.println("Idoso");
-        } else if (idade >= 18) {
-            System.out.println("Maior de idade");
-        } else {
-            System.out.println("Menor de idade");
+        if (nome.contains("ed")) {
+            System.out.println("Contem");
         }
+
+        if (nome.contains("edx") || nome.startsWith("X") || idadeJoao.equals(idadePedro)) {
+            System.out.println("Bem Vindo Pedro");
+        }
+
+        int dia = 10;
+
+        switch (dia) {
+            case 1:
+                System.out.println("Segunda");
+                break;
+            case 2:
+                System.out.println("Terça");
+                break;
+            case 3:
+                System.out.println("Quarta");
+                break;
+            case 4:
+                System.out.println("Quinta");
+                break;
+            case 5:
+                System.out.println("Sexta");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Dia da semana inválido");
+        }
+
+        String diaSemana = "Pedro";
+
+        switch (diaSemana) {
+            case "Segunda":
+                System.out.println("1");
+                break;
+            case "Terça":
+                System.out.println("2");
+                break;
+            case "Quarta":
+                System.out.println("3");
+                break;
+            case "Quinta":
+                System.out.println("4");
+                break;
+            case "Sexta":
+                System.out.println("5");
+                break;
+            case "Sabado":
+                System.out.println("6");
+                break;
+            case "Domingo":
+                System.out.println("7");
+                break;
+            default:
+                System.out.println("Dia da semana inválido");
+        }
+
     }
 }
