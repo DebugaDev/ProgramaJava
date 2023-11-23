a60c9c31429033fbf11ff4aacd9e4ed7188ce92c
Debuga Dev <debugadev@gmail.com>
2023-11-23 07:08:43 -0300

    Laços de Repetição - While e Do While

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        ++ -> Pré-Fixado e o Pós-fixado
-        -- -> Pré-Fixado e o Pós-fixado
-        ! -> Operador complementar (boolean)
-        * */
+        int valor = 50;
 
-        int idade = 0;
+        System.out.println("Inicio While");
 
-        idade = idade + 1;
+        while(valor >= 0) {
+            System.out.println("Valor: " + valor);
 
-        System.out.println("Incremento Idade Pré-Fixado: " + ++idade);
-        System.out.println("Incremento Idade: " + idade++);
-        System.out.println("Incremento Idade Pós-Fixado: " + idade);
-        System.out.println("----------------------------------------");
-        System.out.println("Decremento Idade Pré-Fixado: " + --idade);
-        System.out.println("Decremento Idade: " + idade--);
-        System.out.println("Decremento Idade Pós-Fixado: " + idade);
-        System.out.println("----------------------------------------");
+            valor--;
+        }
 
-        boolean condicao = false;
-        System.out.println("Operador complementar: " + !condicao);
-        System.out.println("----------------------------------------");
+        do {
+            System.out.println("Valor: " + valor);
 
-        idade += 3;
-        System.out.println("Soma: " + idade);
+            valor--;
+        } while(valor >= 0);
 
-        idade -= 2;
-        System.out.println("Subtração: " + idade);
-
-        idade *= 5;
-        System.out.println("Multiplicação: " + idade);
-
-        idade /= 2;
-        System.out.println("Divisão: " + idade);
+        System.out.println("Fim While");
     }
 }
